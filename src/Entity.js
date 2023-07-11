a288d0753be6ff8b4968fb351c59bd1a2b40cbf4
Jeremy Leland <jeremy.leland@gmail.com>
2023-07-11 13:04:12 -0700

    Change direction of player with keyboard (had to fix stroke size calculation)

--- src/Entity.js
+++ src/Entity.js
@@ -1,6 +1,7 @@
 export class Entity {
   x = 0;
   y = 0;
+  angle = 0;
   size = 1;
 
   animationTime = 0;
@@ -12,10 +13,16 @@ export class Entity {
   draw( ctx ) {
     ctx.save();
     ctx.translate( this.x, this.y );
+    ctx.rotate( this.angle );
     ctx.scale( this.size, this.size );
 
     ctx.strokeStyle = 'black';
-    ctx.lineWidth = 1 / ctx.getTransform().m11;
+
+    // https://math.stackexchange.com/questions/13150/extracting-rotation-scale-values-from-2d-transformation-matrix/13165#13165
+    // Assuming uniform scale
+    const t = ctx.getTransform();
+    const sx = /*Math.sign( t.a ) **/ Math.hypot( t.a, t.b );
+    ctx.lineWidth = 1 / sx;
 
     this.drawEntity( ctx );
 
