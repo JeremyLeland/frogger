5bf68cc66091231a93f9db07ded116e3d668e9f6
Jeremy Leland <jeremy.leland@gmail.com>
2024-01-16 23:34:07 -0800

    Simple rasterizing proof of concept for the rest of the entities. (Still only works for loadLevel.html, but it gets 60fps on everything now)

--- src/Entity.js
+++ src/Entity.js
@@ -13,6 +13,8 @@ export const Dir = [
   /*Right:*/  { x:  1, y:  0, angle:  0          , dist: ( x, y ) => Math.floor( x + 1 ) - x },
 ];
 
+export const Rasterized = {};
+
 export class Entity {
   static draw( entity, ctx, { dir, action, time } = {} ) {
     ctx.save();
@@ -20,11 +22,25 @@ export class Entity {
     ctx.rotate( Dir[ ( dir > 0 ? dir : null ) ?? entity.dir ]?.angle ?? 0 );
     // ctx.scale( entity.size, entity.size );   // nothing changes size for now
   
-    ctx.strokeStyle = 'black';
-    ctx.lineWidth = 0.02;
-  
-    Entities[ entity.type ].draw( ctx, action ?? entity.animationAction, time ?? entity.animationTime ?? 0 );
-  
+    if ( !Rasterized[ entity.type ] ) {
+      const offscreen = new OffscreenCanvas( 48 * devicePixelRatio, 48 * devicePixelRatio );
+      const offscreenCtx = offscreen.getContext( '2d' );
+
+      offscreenCtx.scale( 48 * devicePixelRatio, 48 * devicePixelRatio );
+      offscreenCtx.translate( 0.5, 0.5 );
+
+      offscreenCtx.strokeStyle = 'black';
+      offscreenCtx.lineWidth = 0.02;
+
+      Entities[ entity.type ].draw( offscreenCtx, action ?? entity.animationAction, time ?? entity.animationTime ?? 0 );
+
+      Rasterized[ entity.type ] = offscreen;
+    }
+    
+    ctx.translate( -0.5, -0.5 );
+    ctx.scale( 1/48/devicePixelRatio, 1/48/devicePixelRatio );
+    ctx.drawImage( Rasterized[ entity.type ], 0, 0 );
+
     ctx.restore();
   }
 }
