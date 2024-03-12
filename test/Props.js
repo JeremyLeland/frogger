b3cfcfaf4effd6b4e6f607e29c753f3605960b06
Jeremy Leland <jeremy.leland@gmail.com>
2024-03-11 21:04:44 -0700

    Draw lilypad with properly stroked notch (bonus -- shade to distance!)

--- test/Props.js
+++ test/Props.js
@@ -3,20 +3,21 @@ import * as ShaderCommon from '../src/ShaderCommon.js';
 const LILYPAD_COLOR = new Float32Array( [ 0.0, 0.5, 0.0 ] );
 
 const lilypadFrag = ShaderCommon.FragCommon + /*glsl*/`
-  const float LILYPAD_SIZE = 0.4, LILYPAD_ANGLE = 0.3, LILYPAD_OFFSET = 0.15;
+  const float LILYPAD_SIZE = 0.4, LILYPAD_ANGLE = 0.3, LILYPAD_OFFSET = 0.25;
 
   void main() {
-    float angle = atan( v_pos.y, v_pos.x - LILYPAD_OFFSET );
-    float dist = distance( vec2( 0.0 ), v_pos );
+    float fromEdge = LILYPAD_SIZE - distance( vec2( 0.0 ), v_pos );
 
-    // TODO: Mostly flat, with a quick curve off at edges
+    // https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line#Line_defined_by_point_and_angle
+    float lower = -cos( LILYPAD_ANGLE ) * ( 0.0 - v_pos.y ) + sin( LILYPAD_ANGLE ) * ( LILYPAD_OFFSET - v_pos.x );
+    float upper = cos( -LILYPAD_ANGLE ) * ( 0.0 - v_pos.y ) - sin( -LILYPAD_ANGLE ) * ( LILYPAD_OFFSET - v_pos.x );
 
-    // TODO: Fix stroke at notch
+    float dist = min( fromEdge, max( lower, upper ) );
 
-    if ( dist < LILYPAD_SIZE - strokeWidth && abs( angle ) > LILYPAD_ANGLE + strokeWidth ) {
-      outColor = vec4( color, 1.0 );
+    if ( dist > strokeWidth ) {
+      outColor = vec4( mix( BLACK, color, sin( PI / 2.0 * min( 1.0, dist * 9.0 ) ) ), 1.0 );
     }
-    else if ( dist < LILYPAD_SIZE && abs( angle ) > LILYPAD_ANGLE ) {
+    else if ( dist > 0.0 ) {
       outColor = vec4( BLACK, 1.0 );
     }
     else {
@@ -40,6 +41,6 @@ export function drawLilypad( gl, mvp ) {
   ShaderCommon.drawShader( gl, ShaderInfo.Lilypad, {
     mvp: mvp,
     color: LILYPAD_COLOR,
-    strokeWidth: 0.05
+    strokeWidth: 0.03
   } );
 }
